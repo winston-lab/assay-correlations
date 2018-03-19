9f6125eaceaa75e149e4386fe425bfb1cbb4f98b
James Chuang <chuang.ja@gmail.com>
2018-03-19 16:39:52 -0400

    Initial commit.

---
+++ Snakefile
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+configfile: "config.yaml"
+
+REGIONS = config["regions"]
+ASSAYS = config["assays"]
+CONDITIONS = config["conditions"]
+
+localrules: all,
+
+rule all:
+    input:
+        expand("figures/{region}_{condition}_allassays-scatterplots.svg", region=REGIONS, condition=CONDITIONS)
+
+rule make_stranded_annotations:
+    input:
+        lambda wc: REGIONS[wc.region]["annotation"]
+    output:
+        "annotations/{region}/{region}_stranded.bed"
+    shell: """
+        bash scripts/makeStrandedBed.sh {input} | LC_COLLATE=C sort -k1,1 -k2,2n  > {output}
+        """
+
+rule make_modified_annotations:
+    input:
+        annotation = lambda wc: REGIONS[wc.region]["annotation"] if not ASSAYS[wc.assay]["stranded"] else "annotations/" + wc.region + "/" + wc.region + "_stranded.bed",
+        chrsizes = config["genome"]["chrsizes"]
+    output:
+        "annotations/{region}/{region}_{assay}.bed"
+    params:
+        upstream = lambda wc: -(REGIONS[wc.region]["include"][wc.assay]["five_end"]),
+        dnstream = lambda wc: REGIONS[wc.region]["include"][wc.assay]["three_end"]
+    shell: """
+        awk 'BEGIN{{FS=OFS="\t"}} $6=="+"{{print $1, $2, $2+1, $4, $5, $6}} $6=="-"{{print $1, $3-1, $3, $4, $5, $6}}' {input.annotation} | bedtools slop -i stdin -g {input.chrsizes} -l {params.upstream} -r {params.dnstream} -s | LC_COLLATE=C sort -k1,1 -k2,2n > {output}
+        """
+
+rule map_coverage:
+    input:
+        annotation = lambda wc: REGIONS[wc.region]["annotation"] if (not ASSAYS[wc.assay]["stranded"] and REGIONS[wc.region]["include"][wc.assay]["whole-annotation"]) else "annotations/" + wc.region + "/" + wc.region + "_stranded.bed" if REGIONS[wc.region]["include"][wc.assay]["whole-annotation"] else "annotations/" + wc.region + "/" + wc.region + "_" + wc.assay + ".bed",
+        coverage = lambda wc: ASSAYS[wc.assay]["coverage"][wc.sample]["path"]
+    output:
+        "scores/{region}/{region}_{assay}_{sample}.tsv"
+    shell: """
+        LC_COLLATE=C sort -k1,1 -k2,2n {input.annotation} | bedtools map -a stdin -b {input.coverage} -c 4 -o sum | awk 'BEGIN{{FS=OFS="\t"}}{{$7=$7*1000/($3-$2); print $0}}'| sed -e 's/-plus//g;s/-minus//g' | LC_COLLATE=C sort -k1,1 -k2,2n > {output}
+        """
+
+rule average_sample_scores:
+    input:
+        lambda wc: expand("scores/{region}/{region}_{assay}_{sample}.tsv", sample = [k for k,v in ASSAYS[wc.assay]["coverage"].items() if v["group"]==wc.condition], region=wc.region, assay=wc.assay)
+    output:
+        "scores/{region}/{region}_{assay}_{condition}_allsamples.tsv"
+    shell: """
+        cat {input} | LC_COLLATE=C sort -k1,1 -k2,2n -k3,3n -k4,4 -k6,6 | bedtools groupby -g 1-4,6 -c 7 -o mean | awk 'BEGIN{{FS=OFS="\t"}}{{print $1, $2, $3, $4, $6, $5, "{wildcards.assay}"}}' > {output}
+        """
+
+rule cat_assay_scores:
+    input:
+        lambda wc: expand("scores/{region}/{region}_{assay}_{condition}_allsamples.tsv", assay=[k for k,v in ASSAYS.items() if wc.condition in [vv["group"] for kk,vv in ASSAYS[k]["coverage"].items()]], region=wc.region, condition=wc.condition)
+    output:
+        "scores/{region}/{region}_{condition}_allassays.tsv"
+    shell: """
+        cat {input} > {output}
+        """
+
+rule plot_scatter:
+    input:
+        table = "scores/{region}/{region}_{condition}_allassays.tsv"
+    params:
+        pcount = 0.01,
+        anno_label = lambda wc: REGIONS[wc.region]["label"],
+        cutoff_high = lambda wc: REGIONS[wc.region]["cutoff_high"],
+        cutoff_low = lambda wc: REGIONS[wc.region]["cutoff_low"],
+    output:
+        scatter = "figures/{region}_{condition}_allassays-scatterplots.svg"
+    script:
+        "scripts/assay_correlations.R"
+
+
---
+++ scripts/assay_correlations.R
@@ -0,0 +1,102 @@
+library(tidyverse)
+library(forcats)
+library(viridis)
+library(GGally)
+
+scatter = function(intable='coding-genes_WT-37C_allassays.tsv',
+                   pcount=0.01,
+                   anno_label='coding genes',
+                   condition = 'WT-37C',
+                   cutoff_high = 0.999,
+                   cutoff_low = 0.01,
+                   outpath = 'test.svg'){
+    df = read_tsv(intable,
+                  col_names=c('chrom', 'start', 'end', 'name', 'score', 'strand', 'assay')) %>% 
+        mutate(score=score/(end-start),
+               assay = fct_inorder(assay, ordered=TRUE)) %>% 
+        group_by(assay) %>% 
+        mutate(score=log10(score+pcount)) %>% 
+        mutate(score=scale(score))
+    
+    maxsignal = df %>% pull(score) %>% quantile(probs=cutoff_high)
+    minsignal = df %>% pull(score) %>% quantile(probs=cutoff_low)
+    n_assays = n_distinct(df[["assay"]])
+    
+    df = df %>% 
+        select(-c(chrom, start, end, strand)) %>%
+        spread(key=assay, value=score) %>%
+        select(-name)
+        
+    mincor = min(cor(df, use="complete.obs")) * .99
+    plots = list()
+    
+    #for each row
+    for (i in 1:ncol(df)){
+        #for each column
+        for (j in 1:ncol(df)){
+            idx = ncol(df)*(i-1)+j
+            #upper right (correlation)
+            if (i < j){
+                c = cor(df[,i], df[,j], use = "complete.obs") %>% as.numeric()
+                plot = ggplot(data = tibble(x=c(0,1), y=c(0,1), corr=c)) +
+                        geom_rect(aes(fill=corr), xmin=0, ymin=0, xmax=1, ymax=1) +
+                        annotate("text", x=0.5, y=0.5, label=sprintf("%.2f",round(c,2)),
+                                 size=16/72*25.4*c, fontface="plain") +
+                        scale_x_continuous(breaks=NULL) +
+                        scale_y_continuous(breaks=NULL) +
+                        scale_fill_distiller(palette="Blues", limits = c(mincor,1), direction=1)
+                plots[[idx]] = plot
+            }
+            #top left to bot right diag (density)
+            else if (i == j){
+                subdf = df %>% select(i) %>% gather(sample, value)
+                plot = ggplot(data = subdf, aes(x=(value+pcount))) +
+                        geom_density(aes(y=..scaled..), fill="#114477", size=0.8) +
+                        scale_y_continuous(breaks=c(0,.5,1)) +
+                        scale_x_continuous(limits=c(minsignal, maxsignal)) +
+                        annotate("text", x=maxsignal, y=0.9, hjust=1, 
+                                 label=unique(subdf$sample), size=10/72*25.4, fontface="plain") 
+                plots[[idx]] = plot
+            }
+            #bottom left (scatter)
+            else {
+                subdf = df %>% select(i,j) %>% gather(xsample, xvalue, -1) %>%
+                            gather(ysample, yvalue, -c(2:3))
+                plot = ggplot(data = subdf, aes(x=xvalue+pcount, y=yvalue+pcount)) +
+                    geom_abline(intercept = 0, slope=1, color="grey80", size=1) +
+                    stat_bin_hex(geom="point", aes(color=log10(..count..)),
+                    # stat_bin_hex(geom="point", aes(color=..count..),
+                                 binwidth=c(.075,.075), size=0.5, shape=16, stroke=0) +
+                    scale_fill_viridis(option="inferno") +
+                    scale_color_viridis(option="inferno") +
+                    scale_x_continuous(limits = c(minsignal, maxsignal)) +
+                    scale_y_continuous(limits = c(minsignal, maxsignal))
+                plots[[idx]] = plot
+            }
+        }
+    }
+    
+    mat = ggmatrix(plots, nrow=ncol(df), ncol=ncol(df),
+                   title = paste0(nrow(df), " ", anno_label, ", ", condition),
+                   xAxisLabels = names(df), yAxisLabels = names(df), switch="both") +
+        theme_light() +
+        theme(plot.title = element_text(size=12, color="black", face="bold"),
+              axis.text = element_text(size=10, margin=margin(0,0,0,0,"pt")),
+              strip.background = element_blank(),
+              strip.text = element_text(size=12, color="black", face="bold"),
+              strip.text.y = element_text(angle=180, hjust=1),
+              strip.placement="outside")
+    
+    w = 3+n_assays*4
+    h = 9/16*w+0.5
+    ggsave(outpath, plot=mat, width=w, height=h, units="cm", limitsize=FALSE)
+    return(mat)
+}    
+
+plot = scatter(intable = snakemake@input[["table"]],
+               pcount = snakemake@params[["pcount"]],
+               anno_label = snakemake@params[["anno_label"]],
+               condition = snakemake@wildcards[["condition"]],
+               cutoff_high = snakemake@params[["cutoff_high"]],
+               cutoff_low = snakemake@params[["cutoff_low"]],
+               outpath = snakemake@output[["scatter"]])
